fa43aa7b856179abd62adf885716c94e65e3b6a5
csargin <120469159+csargin@users.noreply.github.com>
2024-06-10 14:33:55 +0300

    Update models.py

--- example/models.py
+++ example/models.py
@@ -9,23 +9,9 @@ from django.db import models
 # https://www.techwithtim.net/tutorials/django/sqlite3-database/
 
 
-class Ships(models.Model):
+class Stock(models.Model):
     id = models.AutoField(primary_key=True)
-    ship_name = models.CharField(max_length=100)
-    ship_class= models.CharField(max_length=100)
-    ship_race= models.CharField(max_length=100)
-    ship_price= models.CharField(max_length=100)
-    ship_weapon= models.CharField(max_length=100)
-    ship_turret= models.CharField(max_length=100)
-    ship_hull= models.CharField(max_length=100)
-    ship_cargo= models.CharField(max_length=100)
-    ship_dock= models.CharField(max_length=100)
-    ship_hangar= models.CharField(max_length=100)
-    ship_dlc= models.CharField(max_length=100)
-    ship_role= models.CharField(max_length=100)
-    ship_shield= models.CharField(max_length=100)
-    ship_speed= models.CharField(max_length=100)
-  
+    ticker = models.CharField(max_length = 10)
 
     def __str__(self):
         return self.ticker
