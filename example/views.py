6ef3b1aa06a66110fe11ce69fec7fb9755e414bb
csargin <120469159+csargin@users.noreply.github.com>
2024-06-10 14:14:59 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,16 +1,18 @@
 # example/views.py
 from datetime import datetime
-
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Ships
+import os
 
-def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+def home(request):
+    return render(request, 'home.html',{'api': "" })
+
+def about(request):
+    return render(request, 'about.html',{'api': "" })
+
+def about(request):
+    return render(request, 'about.html',{'api': "" })
+
+def calendar(request):
+    return render(request, 'calendar.html' ,{'api': "" } )
