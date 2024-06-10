a12ea849d884d2088fb5af7af25c2ca362e6e9d5
csargin <120469159+csargin@users.noreply.github.com>
2024-06-10 14:34:44 +0300

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,11 +1,22 @@
-# example/urls.py
-from django.urls import path
-from example import views
-from example.views import home
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from . import views
 
 urlpatterns = [
     path('', views.home, name ="home"),
-    path('about.html', views.about,name ="about"),
+    path('about.html', views.about, name ="about"),
+    path('search.html', views.search, name ="search"),
+    path('add_stock.html', views.add_stock, name ="add_stock"),
+    path('delete/<stock_name>', views.delete, name ="delete"),
+    path('analysis/<stock_name>', views.analysis, name ="analysis"),
+    path('admin/', admin.site.urls),
     path('calendar.html', views.calendar, name ="calendar"),
+    path('forecast/<stock_name>', views.forecast, name ="forecast"),
+    path('generator.html', views.generator, name ="generator"),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
