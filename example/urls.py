b0b9325ef5dd69f175c805acbce6269db02d4efa
csargin <120469159+csargin@users.noreply.github.com>
2024-06-10 14:11:51 +0300

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 # example/urls.py
 from django.urls import path
-
-from example.views import index
-
+from example import views
+from example.views import home
 
 urlpatterns = [
-    path('', index),
+    path('', views.home, name ="home"),
+    path('about.html', views.about,name ="about"),
+    path('calendar.html', views.calendar, name ="calendar"),
+
 ]
